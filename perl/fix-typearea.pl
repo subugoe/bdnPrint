0e5e7f9d08e053c82088cd362353d4369f26c544
MRodz <rodzis@sub.uni-goettingen.de>
2017-11-01 09:26:32 +0100

    added some manual linebreaks that context doesn't handle automatically

--- perl/fix-typearea.pl
+++ perl/fix-typearea.pl
@@ -11,6 +11,8 @@ my $head = "";
 
 my $tail = read_file("tmp/tmp-1.tex");
 
+#$tail =~ s/\x{E2}/--/g;
+
 
 # fixing type area for Noesselt
 $tail =~ s/mathematischen \{\\tfx\\high\{\/c}}Wissenschaften/mathematischen \{\\tfx\\high\{\/c}}Wissen- schaften/g;
@@ -22,9 +24,19 @@ $tail =~ s/\{\\tfx\\high\{\/c}}\\sachIndex\[KIRCHENGESCHICHTE\]\{Kirchengeschich
 $tail =~ s/\{d1e108956}\{\\tfx\\high\{\/c}}\{\/c}derselben/\{d1e108956}\{\\tfx\\high\{\/c}}\{\/c}dersel- ben/g;
 $tail =~ s/genommne\\cNote\{genommene}/genomm- ne\\cNote\{genommene}/g;
 $tail =~ s/im praktischen Verstan/im praktischen Ver-stan/g;
-$tail =~ s/genthümlichen Beschäftigungen/gen-thümlichen Beschäftigungen/g;
-$tail =~ s/\{\\tfx\\high\{\/c\}\}gemeinschaftlicher/\{\\tfx\\high\{\/c\}\}gemeinschaftli-cher/g;
-$tail =~ s/\{\/c\\textbackslash\}derselben\\margin/\{\/c\\textbackslash\}dersel-ben\\margin/g;
+$tail =~ s/\{a246\}genth/\{a246\}gen-th/g;
+#$tail =~ s/\{\\tfx\\high\{\/c\}\}gemeinschaftlicher/\{\\tfx\\high\{\/c\}\}gemeinschaft-licher/g;
+$tail =~ s/348\/47/ 348\/47/g;
+$tail =~ s/1580\) galt/ 1580\) galt/g;
+$tail =~ s/1791\) in kaum/ 1791\) in kaum/g;
+$tail =~ s/1801\) erschien/ 1801\) erschien/g;
+$tail =~ s/1701\) ist 1698/ 1701\) ist 1698/g;
+$tail =~ s/1803\) umfasst/ 1803\) umfasst/g;
+$tail =~ s/1791\) verbinden sich/ 1791\) verbinden sich/g;
+
+
+#doesn't work properly
+$tail =~ s/\{\/c\\textbackslash\}derselben/\{\/c\\textbackslash\}dersel-ben/g;
 
 
 $head = $head . $tail;
