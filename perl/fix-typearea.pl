647c927a9a55b3ebc6ccde6f6c54409ba6942bec
MRodz <rodzis@sub.uni-goettingen.de>
2017-11-01 09:35:08 +0100

    purged superfluous regex exprs

--- perl/fix-typearea.pl
+++ perl/fix-typearea.pl
@@ -17,15 +17,12 @@ my $tail = read_file("tmp/tmp-1.tex");
 # fixing type area for Noesselt
 $tail =~ s/mathematischen \{\\tfx\\high\{\/c}}Wissenschaften/mathematischen \{\\tfx\\high\{\/c}}Wissen- schaften/g;
 $tail =~ s/\{\\tfx\\high\{\/c}}Wissenschaften,\\aNote\{Wissenschaften}/\{\\tfx\\high\{\/c}}Wissen- schaften,\\aNote\{Wissenschaften}/g;
-# PERL has a problem with ä - regex does not match
-#$tail =~ s/\{\\tfx\\high\{\/c}}\\sachIndex\[ERL\QÄ\EUTERUNGSSCHRIFTEN\]\{Erl\Qä\Euterungsschriften}Erl\Qä\Euterungsschriften/\{\\tfx\\high\{\/c}}\\sachIndex\[ERL\QÄ\EUTERUNGSSCHRIFTEN\]\{Erl\Qä\Euterungsschriften}Erl\Qä\Eu- terungsschriften/g;
 $tail =~ s/uterungsschriften kan/u- terungsschriften kan/g;
 $tail =~ s/\{\\tfx\\high\{\/c}}\\sachIndex\[KIRCHENGESCHICHTE\]\{Kirchengeschichte}Kirchengeschichte,/\{\\tfx\\high\{\/c}}\\sachIndex\[KIRCHENGESCHICHTE\]\{Kirchengeschichte}Kirchen- geschichte,/g;
 $tail =~ s/\{d1e108956}\{\\tfx\\high\{\/c}}\{\/c}derselben/\{d1e108956}\{\\tfx\\high\{\/c}}\{\/c}dersel- ben/g;
 $tail =~ s/genommne\\cNote\{genommene}/genomm- ne\\cNote\{genommene}/g;
 $tail =~ s/im praktischen Verstan/im praktischen Ver-stan/g;
 $tail =~ s/\{a246\}genth/\{a246\}gen-th/g;
-#$tail =~ s/\{\\tfx\\high\{\/c\}\}gemeinschaftlicher/\{\\tfx\\high\{\/c\}\}gemeinschaft-licher/g;
 $tail =~ s/348\/47/ 348\/47/g;
 $tail =~ s/1580\) galt/ 1580\) galt/g;
 $tail =~ s/1791\) in kaum/ 1791\) in kaum/g;
