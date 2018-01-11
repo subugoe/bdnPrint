cad9d858b8b32c830f0ae5cb4c67ee52eb9d624d
Michelle Rodzis <rodzis@sub.uni-goettingen.de>
2018-01-11 17:32:02 +0100

    added functionality: pbs in margins are seperated by semicolon unless they belong to the same marker in text

--- perl/postprocess-margins.pl
+++ perl/postprocess-margins.pl
@@ -18,7 +18,6 @@ close(FILE);
 my $tmp1 = read_file("tmp/tmp-1.tex");
 
 
-
 for (my $i = 0; $i < @idFile; $i++) {
   my $id = $idFile[$i];
   chomp($id);
@@ -35,14 +34,45 @@ for (my $i = 0; $i < @idFile; $i++) {
   	
   	$tmp1 =~ s/\\margin\{\}\{e\}\{\}\{\\hbox\{\}\}\{E\}\\pagereference\[(.{7})\]Es war keinesweges/Es\\margin\{\}\{e\}\{\}\{\\hbox\{\}\}\{E\}\\pagereference\[$1\] war keinesweges/g;
   
-    $tmp1 =~ s/(\\margin\{$id\}\{.*?\}\{.*?\}\{.*?\}\{.*?\})/$1\\margindata\[inouter\]\{$note\}/g;
+	$tmp1 =~ s/(\\margin\{$id\}\{.*?\}\{.*?\}\{.*?\}\{.*?\})/$1\\margindata\[inouter\]\{$note\}/g;
     
-    $tmp1 =~ s/\\margindata\[inouter\]\{, /\\margindata\[inouter\]\{/g;
-    
-    $tmp1 =~ s/;,/;/g;
+   	$tmp1 =~ s/\\margindata\[inouter\]\{, /\\margindata\[inouter\]\{/g;
+    	$tmp1 =~ s/([\w]{1}[\[]{0,1}[0-9IVX]{1,3}[\]]{0,1}), ([\w]{1}[\[]{0,1}[0-9IVX]{1,3}[\]]{0,1})/$1; $2/g;     
+
+    	$tmp1 =~ s/;,/;/g;
+	
+
+	# in the cases where there are several pagebreaks in one margin line, try to find out if markers in margin belong to the same
+	# pagebreak or not. when several pagebreaks collide into one \vl, the hidden pagebreaks have an \hbox as 4th argument of \margin.
+	# in these cases, the pagebreak markes in the margin should be seperated by a comma, otherwise by a semicolon.
+	if($note =~ m/([\w]{1}[\[]{0,1}[0-9XVI]{1,4}[\]]{0,1}), ([\w]{1}[\[]{0,1}[0-9XVI]{1,4}[\]]{0,1})/) {
+		my @pbNotes = split(', ', $note);
+
+		for(my $j = 0; $j < @pbNotes; $j++) {
+			my $pb = $pbNotes[$j];
+
+			# workaround, otherwise [ and ] are treated as part of regex
+			$tmp1 =~ s/\[([0-9XIV]*?)\]/!$1!/g;
+			$pb =~ s/\[([0-9XIV]*?)\]/!$1!/g;
+
+			# when several pagebreaks in margin belong to the same marker im main text, set comma			
+			if($tmp1 =~ m/\\margin\{[\w]{8}\}\{pb\}\{\}\{\\hbox\{\}\}\{$pb\}/) {
+				my ($edition) = $pb =~ /([\w])/;
+				my ($page) = $pb =~ /([!]{0,1}[0-9XVI]{1,4}[!]{0,1})/;
+				# markes : is needed, otherwise , will be substituted by ; in for loop
+				$tmp1 =~ s/; $pb/, $edition:$page/g;
+				# remove superfluous \margin
+				$tmp1 =~ s/\\margin\{[\w]{8}\}\{pb\}\{\}\{\\hbox\{\}\}\{$pb\}//g;
+			}
+		}
+	}
   }
-  print STDERR "NOTE: " . $note . "\n";
 }
+
+# remove marker
+$tmp1 =~ s/, ([\w]):([!]{0,1}[0-9XVI]{1,4}[!]{0,1})/, $1$2/g;
+# replace !...! with [...]
+$tmp1 =~ s/!([0-9XIV]*?)!/\[$1\]/g;
 
 print $tmp1;
 
