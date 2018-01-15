67b0f6fc7785169933efa8dc6d026eeeaaafb8bb
Michelle Rodzis <rodzis@sub.uni-goettingen.de>
2018-01-15 12:00:52 +0100

    replaces \bs with \textbackslash

--- perl/merge-markers.pl
+++ perl/merge-markers.pl
@@ -32,23 +32,23 @@ while ($tmp1 =~ /(\\high\{\/[a-i]{0,8}\}[^~ ]*\\high\{\/[a-i]{0,8}\})/g) {
   $tmp1 =~ s/$search/$replace/;
 }
 
-while ($tmp1 =~ /(\\high\{[a-i]{0,8}\\bs\}[^~ ]*\\high\{[a-i]{0,8}\\bs\})/g) {
+while ($tmp1 =~ /(\\high\{[a-i]{0,8}\\textbackslash\}[^~ ]*\\high\{[a-i]{0,8}\\textbackslash\})/g) {
   my $search = $1;
   my $replace = $1;
 
   my $wits1 = $1;
   my $wits2 = $1;
 
-  $wits1 =~ s/.*?\\high\{([a-i]{0,8})\\bs\}.*/$1/;
-  $wits2 =~ s/.*\\high\{([a-i]{0,8})\\bs\}.*?/$1/;
+  $wits1 =~ s/.*?\\high\{([a-i]{0,8})\\textbackslash\}.*/$1/;
+  $wits2 =~ s/.*\\high\{([a-i]{0,8})\\textbackslash\}.*?/$1/;
 
   my $wits = $wits1 . $wits2;
   my @wits = split("", $wits);
   @wits = sort(uniq(@wits));
   $wits = join("", @wits);
 
-  $replace =~ s/\\high\{[a-i]{0,8}\\bs\}/\\high\{$wits\\bs\}/;
-  $replace =~ s/(.*)\\high\{[a-i]{0,8}\\bs\}/$1\\hbox\{\}/;
+  $replace =~ s/\\high\{[a-i]{0,8}\\textbackslash\}/\\high\{$wits\\textbackslash\}/;
+  $replace =~ s/(.*)\\high\{[a-i]{0,8}\\textbackslash\}/$1\\hbox\{\}/;
 
   $search = quotemeta($search);
   $tmp1 =~ s/$search/$replace/;
