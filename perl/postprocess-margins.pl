a4ed9d24c21faff8dcd449bd58f067d5af330243
Michelle Rodzis <rodzis@sub.uni-goettingen.de>
2018-01-18 15:57:48 +0100

    final implementation for pagebreaks in margins. further development of handling overfull margins

--- perl/postprocess-margins.pl
+++ perl/postprocess-margins.pl
@@ -18,11 +18,9 @@ close(FILE);
 my $file_location = "tmp/moved_elements.txt";
 open(my $file, ">", $file_location) or die $!;
 
-
 my $tmp1 = read_file("tmp/" . $ARGV[0] . "_tmp-1.tex");
 
 my $moveIntoNextMargindata = "";
-
 
 for (my $i = 0; $i < @idFile; $i++) {
 	# workaround, otherwise [ and ] are treated as part of regex
@@ -34,67 +32,69 @@ for (my $i = 0; $i < @idFile; $i++) {
   chomp($note);
 
   if ($id and $note) {
+		# workaround, otherwise [ and ] are treated as part of regex
 		if($note =~ m/\[[\dXVI]+?\]/) {
 			$note =~ s/\[([\dXVI]+?)\]/!$1!/g;
 		}
-	# temporary solution for merging note indicators. @TODO improve
- 	$tmp1 =~ s/E,\sE/E/g;
-	$tmp1 =~ s/E(,\s.{2,4})+,\sE/E$1/g;
-	$tmp1 =~ s/E(,\s\/a\\textbackslash)+,\sE/E$1/g;
-	$tmp1 =~ s/E(,\s\/c\\textbackslash)+,\sE/E$1/g;
+
+		# temporary solution for merging note indicators. @TODO improve
+ 		$tmp1 =~ s/E,\sE/E/g;
+		$tmp1 =~ s/E(,\s.{2,4})+,\sE/E$1/g;
+		$tmp1 =~ s/E(,\s\/a\\textbackslash)+,\sE/E$1/g;
+		$tmp1 =~ s/E(,\s\/c\\textbackslash)+,\sE/E$1/g;
   	
-	$tmp1 =~ s/\\margin\{\}\{e\}\{\}\{\\hbox\{\}\}\{E\}\\pagereference\[(.{7})\]Es war keinesweges/Es\\margin\{\}\{e\}\{\}\{\\hbox\{\}\}\{E\}\\pagereference\[$1\] war keinesweges/g;
+		$tmp1 =~ s/\\margin\{\}\{e\}\{\}\{\\hbox\{\}\}\{E\}\\pagereference\[(.{7})\]Es war keinesweges/Es\\margin\{\}\{e\}\{\}\{\\hbox\{\}\}\{E\}\\pagereference\[$1\] war keinesweges/g;
  
-	if($moveIntoNextMargindata =~ m/[\w]/) {
-		# remove pb from original \margindata[inouter] to the current one
-		$tmp1 =~ s/(\\margin\{$id\}\{.*?\}\{.*?\}\{.*?\}\{)/$1$moveIntoNextMargindata, /g;
-		$tmp1 =~ s/(\\margin\{$id\}\{.*?\}\{.*?\}\{.*?\}\{.*?\})/$1\\margindata\[inouter\]\{$moveIntoNextMargindata\], $note\}/g;
-		$moveIntoNextMargindata = "";
-	} 
-	else {
-		$tmp1 =~ s/(\\margin\{$id\}\{.*?\}\{.*?\}\{.*?\}\{.*?\})/$1\\margindata\[inouter\]\{$note\}/g;
-	}
-	$tmp1 =~ s/([\w]{1}[!]{0,1}[0-9IVX]{1,3}[!]{0,1}), ([\w]{1}[!]{0,1}[0-9IVX]{1,3}[!]{0,1})/$1; $2/g;     
-	
+		if($moveIntoNextMargindata =~ m/[\w]/) {
+			# move pb from original \margindata[inouter] to the current one
+			$tmp1 =~ s/(\\margin\{$id\}\{.*?\}\{.*?\}\{.*?\}\{)/$1$moveIntoNextMargindata, /g;
+			$tmp1 =~ s/(\\margin\{$id\}\{.*?\}\{.*?\}\{.*?\}\{.*?\})/$1\\margindata\[inouter\]\{$moveIntoNextMargindata\], $note\}/g;
+			$moveIntoNextMargindata = "";
+		} 
+		else {
+			$tmp1 =~ s/(\\margin\{$id\}\{.*?\}\{.*?\}\{.*?\}\{.*?\})/$1\\margindata\[inouter\]\{$note\}/g;
+		}
+		$tmp1 =~ s/([\w]{1}[!]{0,1}[0-9IVX]{1,7}[!]{0,1}), ([\w]{1}[!]{0,1}[0-9IVX]{1,7}[!]{0,1})/$1; $2/g;     
 
-	# in the cases where there are several pagebreaks in one margin line, try to find out if markers in margin belong to the same
-	# pagebreak or not. when several pagebreaks collide into one \vl, the hidden pagebreaks have an \hbox as 4th argument of \margin.
-	# in these cases, the pagebreak markers in the margin should be seperated by a comma, otherwise by a semicolon.
-	if($note =~ m/([\w]{1}[!]{0,1}[0-9XVI]{1,4}[!]{0,1}), ([\w]{1}[!]{0,1}[0-9XVI]{1,4}[!]{0,1})/) {
-		my @pbNotes = split(', ', $note);
-		
 		# prepare analyzation of notelength
-		$note =~ s/\\textbackslash/\\\&/g;
-		my $notesWithoutLastPb = join '', @pbNotes[0 .. $#pbNotes-1];
-		my $sizeBeforeLastPb = length($notesWithoutLastPb);
-		# undo changes for analyzation
-		$note =~ s/\\\&/\\textbackslash/g;
+		$note =~ s/\\textbackslash/\\#/g;
+		my @marginElements = split(', ', $note);
 
-		for(my $j = 0; $j < @pbNotes; $j++) {
-			my $pb = $pbNotes[$j];
+		my $noOfElements = @marginElements;
+		my $marginSize = length($note);
 
-			# workaround, otherwise [ and ] are treated as part of regex
-			$pb =~ s/\[([0-9XIV]*?)\]/!$1!/g;
+		# look at all margin elements in $note
+		for(my $j = 0; $j < @marginElements; $j++) {
+			my $element = $marginElements[$j];
 
-			# when several pagebreaks in margin belong to the same marker in main text, set comma			
-			if($tmp1 =~ m/\\margin\{[\w]{8}\}\{pb\}\{\}\{\\hbox\{\}\}\{$pb\}/) {
-				my ($edition) = $pb =~ /([\w])/;
-				my ($page) = $pb =~ /([!]{0,1}[0-9XVI]{1,5}[!]{0,1})/;
-				# ":" is needed, otherwise "," will be substituted by ";" in for loop
-				$tmp1 =~ s/; $pb/, $edition:$page/g;
+			# in the cases where there are several pagebreaks in one margin line, try to find out if markers in margin belong to the same
+			# pagebreak or not. when several pagebreaks collide into one \vl, the hidden pagebreaks have an \hbox{} as 4th argument of \margin.
+			# in these cases, the pagebreak markers in the margin should be seperated by a comma, otherwise by a semicolon.			
+			if($element =~ m/[\w]{1}[!]{0,1}[0-9XVI]{1,7}[!]{0,1}/ 
+			and $tmp1 =~ m/\\margin\{[\w]{8}\}\{pb\}\{\}\{\\hbox\{\}\}\{$element\}/) {
+					my ($edition) = $element =~ /([\w])/;
+					my ($page) = $element =~ /([!]{0,1}[0-9XVI]{1,7}[!]{0,1})/;
+					# ":" is needed, otherwise "," will be substituted by ";" in for loop
+					$tmp1 =~ s/[\,\;] $element/, $edition:$page/g;			
 			}
 
 			#@TODO documentation! may also occur in margin notes, where last pb is the only one (isn't considered at the moment
-			if(@pbNotes - 1 == $j and $pb =~ m/[\w][!]{0,1}[\dXIV]{1,5}[!]{0,1}/
+			if($noOfElements > 1 and $noOfElements - 1 == $j
+			# has to be adjusted: only 7 for large entries before. has to be larger for small entries.
+			and $marginSize >= 17
 			# assure that the following line has content in margin, too
 			and $notes[$i+1] =~ m/[\w]/
-			# has to be adjusted: only 7 for large entries before. has to be larger for small entries.
-			and $sizeBeforeLastPb >= 7) {
-				$moveIntoNextMargindata = $pb;
-				print $file $pb . "\n";
-			}
+			# experience shows that removal is only necessary when we have >= 2 pagebreaks
+			and $note =~ m/[a-z][!]{0,1}[0-9XVI]{1,7}[!]{0,1}.*?[a-z]{1}[!]{0,1}[0-9XVI]{1,7}[!]{0,1}/) {
+				$note =~ s/\\#/\\textbackslash/g;
+				$element =~ s/\\#/\\textbackslash/g;
+				$moveIntoNextMargindata = $element;
+				print $file $note . "\n";
+			}				
 		}
-	}
+
+		# undo changes for analyzation
+		$note =~ s/\\#/\\textbackslash/g;
   }
 }
 
--- perl/remove-moved-elements.pl
+++ perl/remove-moved-elements.pl
@@ -8,20 +8,30 @@ use warnings;
 use File::Slurp;
 
 open(FILE, "<tmp/moved_elements.txt") or die "$!\n";
-my @elements = <FILE>;
+my @notes = <FILE>;
 close(FILE);
 
 my $tmp = read_file("tmp/" . $ARGV[0] . "_tmp-3.tex");
 
-for (my $i = 0; $i < @elements; $i++) {
-  my $element = $elements[$i];
-  chomp($element);
+for (my $i = 0; $i < @notes; $i++) {
+  my $note = $notes[$i];
+  chomp($note);
+
+	my @elements = split(', ', $note);
+	my $context = join ', ', @elements[0 .. $#elements-1];
+	my $element = $elements[-1];
+
+	print STDERR $note . "\n";
+	print STDERR $element . "\n";	
+	print STDERR $context . "\n";
 
 	my ($edition) = $element =~ /([a-z])/;
 	my ($page) = $element =~ /([\dXVI]+)/; 
-	my $pattern = " " . $element . "\}";
 
-	$tmp =~ s/$pattern/\}/g;
+	# note has to be used as pattern BUT: we have /a\textbackslash etc --> slashes are interpreted as part of regex! @TODO
+	my $patternTmp = " " . $element . "\}";
+
+	#$tmp =~ s/$pattern/\}/g;
 }
 
 # replace !...! with [...]
