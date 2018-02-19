3b55ae7d3f073ac0a98ee9b8aa9975f41903de99
Michelle Rodzis <rodzis@sub.uni-goettingen.de>
2018-02-19 13:09:15 +0100

    added differentiation between pb margin notes that refer to the same marker and those that refer to different markers

--- perl/core/postprocess-margins.pl
+++ perl/core/postprocess-margins.pl
@@ -38,12 +38,10 @@ for (my $i = 0; $i < @idFile; $i++) {
 		}
 
 		# temporary solution for merging note indicators. @TODO improve
- 		$tmp1 =~ s/E,\sE/E/g;
-		$tmp1 =~ s/E(,\s.{2,4})+,\sE/E$1/g;
+ 		#$tmp1 =~ s/E,\sE/E/g;
+		$tmp1 =~ s/E(,.*?)+,\sE/E$1/g;
 		$tmp1 =~ s/E(,\s\/a\\textbackslash)+,\sE/E$1/g;
 		$tmp1 =~ s/E(,\s\/c\\textbackslash)+,\sE/E$1/g;
-  	
-		$tmp1 =~ s/\\margin\{\}\{e\}\{\}\{\\hbox\{\}\}\{E\}\\pagereference\[(.{7})\]Es war keinesweges/Es\\margin\{\}\{e\}\{\}\{\\hbox\{\}\}\{E\}\\pagereference\[$1\] war keinesweges/g;
  
 		if($moveIntoNextMargindata =~ m/[\w]/) {
 			# move pb from original \margindata[inouter] to the current one
@@ -54,16 +52,20 @@ for (my $i = 0; $i < @idFile; $i++) {
 		else {
 			$tmp1 =~ s/(\\margin\{$id\}\{.*?\}\{.*?\}\{.*?\}\{.*?\})/$1\\margindata\[inouter\]\{$note\}/g;
 		}
-		$tmp1 =~ s/([\w]{1}[!]{0,1}[0-9IVX]{1,7}[!]{0,1}), ([\w]{1}[!]{0,1}[0-9IVX]{1,7}[!]{0,1})/$1; $2/g;       
+		$tmp1 =~ s/(\\margindata\[inouter\]\{[[\/]{0,1}[\w]{0,5}[\\textbackslash]{0,1}[,]{0,1}]*?)([\w]{1}[!]{0,1}[0-9IVX]{1,7}[!]{0,1}), ([\w]{1}[!]{0,1}[0-9IVX]{1,7}[!]{0,1})/$1$2; $3/g;       
 
 		# prepare analyzation of notelength
-		$note =~ s/\\textbackslash/\\#/g;
+		$note =~ s/\\textbackslash/\\@/g;
+		#$tmp1 =~ s/\\textbackslash/\\@/g;
 		my @marginElements = split(', ', $note);
 
 		my $noOfElements = @marginElements;
 		my $marginSize = length($note);
 
-		$note =~ s/([\w]{1}[!]{0,1}[0-9IVX]{1,7}[!]{0,1}), ([\w]{1}[!]{0,1}[0-9IVX]{1,7}[!]{0,1})/$1; $2/g;
+# Luc. 16; 23–25.
+
+		$note =~ s/([a-z]{1}[!]{0,1}[0-9IVX]{1,7}[!]{0,1}), ([a-z]{1}[!]{0,1}[0-9IVX]{1,7}[!]{0,1})/$1; $2/g;
+		$tmp1 =~ s/([a-z]{1}[!]{0,1}[0-9IVX]{1,7}[!]{0,1}), ([a-z]{1}[!]{0,1}[0-9IVX]{1,7}[!]{0,1})/$1; $2/g;
 
 		# look at all margin elements in $note
 		for(my $j = 0; $j < @marginElements; $j++) {
@@ -81,10 +83,31 @@ for (my $i = 0; $i < @idFile; $i++) {
 					$note =~ s/[\,\;] $element/, $edition:$page/g;
 			}
 
-			if($noOfElements > 1 and $noOfElements - 1 == $j
+
+
+			if(($note =~ m/[\w]{1}[!]{0,1}[0-9XVI]{3,7}[!]{0,1}/
+			or $note =~ m/[\w]{1}[!]{1}[0-9XVI]{2,7}[!]{1}/)
+			and $noOfElements > 1 and $noOfElements - 1 == $j
 			and $marginSize >= 17
-			and $notes[$i+1] =~ m/[^\w]/) {
-				# @TODO eckige Klammer nach dem letzten Element
+			and $notes[$i+1] !~ m/[\w]/
+			# experience shows that removal is only necessary when we have >= 2 pagebreaks and 2 >= scribal abbreviations
+			# at least two pagebreaks with sp and roman numbers ...
+			and ($note =~ m/[\w]{1}[!]{1}[0-9XVI]{1,7}[!]{1}.*?[\w]{1}[!]{1}[0-9XVI]{1,7}[!]{1}/)
+			# ... or at least two normal pbs and at least two scribal abbreviations
+				or ($note =~ m/[a-z][0-9]{3,7}.*?[a-z]{1}[:]{0,1}[0-9]{3,7}/
+				and ($note =~ m/[\/][\w].*?[\/][\w]/
+				or $note =~ m/[\/][\w].*?[\w][\\]/
+				or $note =~ m/[\w][\\].*?[\/][\w]/
+				or $note =~ m/[\w][\\].*?[\w][\\]/))) {
+					#$note =~ s/([\/\\])/\\$1/g;
+					#$tmp1 =~ s/$note/$note\]/g;
+					#$note = $note . "\]";
+					#$note =~ s/[\/\\]/\\/g;
+					my $pattern = $note;
+					$pattern =~ s/([\/\\;])/\\$1/g;
+					$tmp1 =~ s/$note/$note\]/g;
+					print STDERR $note . "\n";
+					print STDERR $pattern . "\n";
 			}
 
 			#@TODO documentation! may also occur in margin notes, where last pb is the only one (isn't considered at the moment
@@ -94,16 +117,18 @@ for (my $i = 0; $i < @idFile; $i++) {
 			and $notes[$i+1] =~ m/[\w]/
 			# experience shows that removal is only necessary when we have >= 2 pagebreaks
 			and $note =~ m/[a-z][!]{0,1}[0-9XVI]{1,7}[!]{0,1}.*?[a-z]{1}[:]{0,1}[!]{0,1}[0-9XVI]{1,7}[!]{0,1}/) {
-				$note =~ s/\\#/\\textbackslash/g;
+				$note =~ s/\\@/\\textbackslash/g;
 				$note =~ s/://g;
-				$element =~ s/\\#/\\textbackslash/g;
+				$element =~ s/\\@/\\textbackslash/g;
 				$moveIntoNextMargindata = $element;
 				print $file $note . "\n";
 			}				
 		}
 
 		# undo changes for analyzation
-		$note =~ s/\\#/\\textbackslash/g;
+		$note =~ s/\\@/\\textbackslash/g;
+		#$tmp1 =~ s/\\@/\\textbackslash/g;
+		#$tmp1 =~ s/textbackslash/\\textbackslash/g;
   }
 }
 
