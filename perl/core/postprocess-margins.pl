141446cde3c7105d4446c675074bb6ed61f02353
Michelle Rodzis <rodzis@sub.uni-goettingen.de>
2018-03-22 15:44:32 +0100

    simplified handling of margin data. deleting superfluous (TeX) code)

--- perl/core/postprocess-margins.pl
+++ perl/core/postprocess-margins.pl
@@ -7,6 +7,34 @@ use warnings;
 
 use File::Slurp;
 
+my $tmp1 = read_file("tmp/" . $ARGV[0] . "_tmp-1.tex");
+
+### Editorial comments
+
+open(FILE, "<tmp/comments.txt") or die "$!\n";
+my @commentsFile = <FILE>;
+close(FILE);
+
+# iterate lines in document
+for (my $i = 0; $i < @commentsFile; $i++) {
+	my $comments = $commentsFile[$i];
+	
+	if($comments and $comments =~ m/\s/) {
+		# iterate single comments
+		# apart from the first comment every margin entry of an editorial comment is deleted in $tmp1,
+		# hence we start at $j = 1 instead of $j = 0 
+		my @commentElements = split(' ', $comments);
+		for (my $j = 1; $j < @commentElements; $j++) {
+			my $comment = $commentElements[$j];
+
+			$tmp1 =~ s/\\margin\{$comment\}\{e\}\{\}\{\\hbox\{\}\}\{E\}//g;
+		}
+	}
+}
+
+
+### Pagebreaks and general margin data
+
 open(FILE, "<tmp/id-file.txt") or die "$!\n";
 my @idFile = <FILE>;
 close(FILE);
@@ -17,8 +45,6 @@ close(FILE);
 
 my $file_location = "tmp/moved_elements.txt";
 open(my $file, ">", $file_location) or die $!;
-
-my $tmp1 = read_file("tmp/" . $ARGV[0] . "_tmp-1.tex");
 
 my $moveIntoNextMargindata = "";
 
@@ -36,12 +62,6 @@ for (my $i = 0; $i < @idFile; $i++) {
 		if($note =~ m/\[[\dXVI]+?\]/) {
 			$note =~ s/\[([\dXVI]+?)\]/!$1!/g;
 		}
-
-		# temporary solution for merging note indicators. @TODO improve
- 		#$tmp1 =~ s/E,\sE/E/g;
-		$tmp1 =~ s/E(,.*?)+,\sE/E$1/g;
-		$tmp1 =~ s/E(,\s\/a\\textbackslash)+,\sE/E$1/g;
-		$tmp1 =~ s/E(,\s\/c\\textbackslash)+,\sE/E$1/g;
  
 		if($moveIntoNextMargindata =~ m/[\w]/) {
 			# move pb from original \margindata[inouter] to the current one
@@ -62,8 +82,10 @@ for (my $i = 0; $i < @idFile; $i++) {
 		my $noOfElements = @marginElements;
 		my $marginSize = length($note);
 
-# Luc. 16; 23–25.
+		# undo changes for analyzation
+		$note =~ s/\\@/\\textbackslash/g;
 
+		# Luc. 16; 23–25.
 		$note =~ s/([a-z]{1}[!]{0,1}[0-9IVX]{1,7}[!]{0,1}), ([a-z]{1}[!]{0,1}[0-9IVX]{1,7}[!]{0,1})/$1; $2/g;
 		$tmp1 =~ s/([a-z]{1}[!]{0,1}[0-9IVX]{1,7}[!]{0,1}), ([a-z]{1}[!]{0,1}[0-9IVX]{1,7}[!]{0,1})/$1; $2/g;
 
@@ -84,32 +106,30 @@ for (my $i = 0; $i < @idFile; $i++) {
 			}
 
 
-
-			if(($note =~ m/[\w]{1}[!]{0,1}[0-9XVI]{3,7}[!]{0,1}/
-			or $note =~ m/[\w]{1}[!]{1}[0-9XVI]{2,7}[!]{1}/)
-			and $noOfElements > 1 and $noOfElements - 1 == $j
-			and $marginSize >= 17
-			and $notes[$i+1] !~ m/[\w]/
-			# experience shows that removal is only necessary when we have >= 2 pagebreaks and 2 >= scribal abbreviations
-			# at least two pagebreaks with sp and roman numbers ...
-			and ($note =~ m/[\w]{1}[!]{1}[0-9XVI]{1,7}[!]{1}.*?[\w]{1}[!]{1}[0-9XVI]{1,7}[!]{1}/)
-			# ... or at least two normal pbs and at least two scribal abbreviations
-				or ($note =~ m/[a-z][0-9]{3,7}.*?[a-z]{1}[:]{0,1}[0-9]{3,7}/
-				and ($note =~ m/[\/][\w].*?[\/][\w]/
-				or $note =~ m/[\/][\w].*?[\w][\\]/
-				or $note =~ m/[\w][\\].*?[\/][\w]/
-				or $note =~ m/[\w][\\].*?[\w][\\]/))) {
-					#$note =~ s/([\/\\])/\\$1/g;
-					#$tmp1 =~ s/$note/$note\]/g;
-					#$note = $note . "\]";
-					#$note =~ s/[\/\\]/\\/g;
+			# case 1: a few small elements. doesn't result in a linebreak, no code needed.
+			# case 2: a lot of small or a few large elements while the next margin line is empty. then we
+			# only have to append \].
+			if($notes[$i+1] !~ m/[\w]/ and $noOfElements - 1 == $j
+			# estimation: either a lot of small elements ...
+			and (($marginSize >= 20 and $noOfElements >= 3) 
+				# ... or a few large elements. 17 is gained from experience, 22 is the
+				# maximum size a combination of two elements can have (e.g. c[XXXVIII], d[XXXVIII])
+				or ($marginSize >= 16 and $marginSize <= 22 and $noOfElements < 3))) {
+					# workaround: if the pb doesn't have an hbox, the semicolon would otherwise be set in the next
+					# loop iteration -- which would prevent \] from being appended
+					if($element =~ m/[\w]{1}[!]{0,1}[0-9XVI]{1,7}[!]{0,1}/ and
+					$tmp1 !~ m/\\margin\{[\w]{8}\}\{pb\}\{\}\{\\hbox\{\}\}\{$element\}/) {
+						$tmp1 =~ s/, $element/; $element/g;
+						$note =~ s/, $element/; $element/g;
+					} 
 					my $pattern = $note;
 					$pattern =~ s/([\/\\;])/\\$1/g;
-					$tmp1 =~ s/$note/$note\]/g;
-					print STDERR $note . "\n";
-					print STDERR $pattern . "\n";
+					$tmp1 =~ s/$pattern/$note\]/g;
+				
 			}
 
+			# case 3: a lot of small or a few large elements while the next margin line is NOT empty. then the last element
+			# has to be copied into the next \\margindata[inouter] at the beginning of the next loop iteration.
 			#@TODO documentation! may also occur in margin notes, where last pb is the only one (isn't considered at the moment
 			if($noOfElements > 1 and $noOfElements - 1 == $j
 			and $marginSize >= 17
@@ -117,18 +137,14 @@ for (my $i = 0; $i < @idFile; $i++) {
 			and $notes[$i+1] =~ m/[\w]/
 			# experience shows that removal is only necessary when we have >= 2 pagebreaks
 			and $note =~ m/[a-z][!]{0,1}[0-9XVI]{1,7}[!]{0,1}.*?[a-z]{1}[:]{0,1}[!]{0,1}[0-9XVI]{1,7}[!]{0,1}/) {
-				$note =~ s/\\@/\\textbackslash/g;
 				$note =~ s/://g;
 				$element =~ s/\\@/\\textbackslash/g;
 				$moveIntoNextMargindata = $element;
 				print $file $note . "\n";
-			}				
+			}	else {
+				$moveIntoNextMargindata = "";
+			}	
 		}
-
-		# undo changes for analyzation
-		$note =~ s/\\@/\\textbackslash/g;
-		#$tmp1 =~ s/\\@/\\textbackslash/g;
-		#$tmp1 =~ s/textbackslash/\\textbackslash/g;
   }
 }
 
@@ -137,7 +153,10 @@ close $file;
 # remove marker
 $tmp1 =~ s/, ([\w]):([!]{0,1}[0-9XVI]{1,4}[!]{0,1})/, $1$2/g;
 # remove superfluous \margin
-$tmp1 =~ s/\\margin\{[\w]{8}\}\{pb\}\{\}\{\\hbox\{\}\}\{.*?\}//g;
+$tmp1 =~ s/ \\margin\{[\w]{8}\}\{pb\}\{\}\{\\hbox\{\}\}\{.*?\}//g;
+# merging note indicators
+$tmp1 =~ s/E,\sE/E/g;
+$tmp1 =~ s/E, ([\w\\\d\s,]+) E/E, $1/g;
 
 print $tmp1;
 
