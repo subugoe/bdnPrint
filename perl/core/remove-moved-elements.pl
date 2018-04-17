de9522f06784da6f708070bfc33fd4ec4f947419
Michelle Rodzis <rodzis@sub.uni-goettingen.de>
2018-04-17 14:44:07 +0200

    reading right file

--- perl/core/remove-moved-elements.pl
+++ perl/core/remove-moved-elements.pl
@@ -11,7 +11,7 @@ open(FILE, "<tmp/moved_elements.txt") or die "$!\n";
 my @notes = <FILE>;
 close(FILE);
 
-my $tmp = read_file("tmp/" . $ARGV[0] . "_tmp-3.tex");
+my $tmp = read_file("tmp/" . $ARGV[0] . "_tmp-4.tex");
 
 for (my $i = 0; $i < @notes; $i++) {
   my $note = $notes[$i];
@@ -20,7 +20,7 @@ for (my $i = 0; $i < @notes; $i++) {
 	my @elements = split(', ', $note);
 	my $element = $elements[-1];
 	my ($context) = $note =~ /(.*) $element/;
-	# has to escaped because of regex
+	# has to be escaped because of regex
 	$note =~ s/([\/\\])/\\$1/g;
 
 	my $pattern = $note . "\}";
