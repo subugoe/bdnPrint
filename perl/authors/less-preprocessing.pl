18156a55eecde4bd4caf7a336ed6b0e7f923d2c3
Michelle Rodzis <rodzis@sub.uni-goettingen.de>
2018-04-18 11:44:47 +0200

    first commit

--- perl/authors/less-preprocessing.pl
+++ perl/authors/less-preprocessing.pl
@@ -11,6 +11,9 @@ my $head = "";
 
 my $tail = read_file("tmp/less_tmp-1.tex");
 
+#$tail =~ s/\\midaligned\{\\margin\{\}\{pb\}\{\}\{\\vl\}\{([a-z][\[]{0,1}[0-9XVI]{1,7}[\]]{0,1})\}/\\margin\{\}\{pb\}\{\}\{\\vl\}\{$1\}\\midaligned\{/g;
+
+
 
 $head = $head . $tail;
 print $head;
