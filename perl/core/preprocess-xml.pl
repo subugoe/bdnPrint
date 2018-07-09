57319465c7db14fd7e960afeb5045b83b0437531
Michelle Rodzis <rodzis@sub.uni-goettingen.de>
2018-07-09 10:32:31 +0200

    Add handling of Hebrew

--- perl/core/preprocess-xml.pl
+++ perl/core/preprocess-xml.pl
@@ -11,7 +11,9 @@ my $tmp = read_file("" . $ARGV[0] . "_tmp.xml");
 
 $tmp =~ s/@/ /g;
 
-# rdgMarkers
-#$tmp =~ s/(rdgMarker wit=".*?" type=".*?" ref=".*?" mark="open" .*?"\/\>)(\%*\s*|@@)/$1/g;
+# round brackets around Hebrew words have to be switched inside the foreign element, because otherwise the bidirectional
+# flow causes the brackets to be rendered before the Hebrew word, e.g. ()‫ם‬ ‫י‬ ‫א‬ ‫ד‬ ‫ו‬ ‫ד‬  instead of (‫ם‬ ‫י‬ ‫א‬ ‫ד‬ ‫ו‬ ‫ד‬). same holds for
+# punctuation.
+$tmp =~ s/\((\<foreign xml:lang="he".{0,18}\>)(.*?)(\<\/foreign\>)\)/$1\($2\)$3/g;
 
 print $tmp;
